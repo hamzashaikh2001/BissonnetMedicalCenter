17e06551c12cc1a7c8183caf54c8fd8b43f9f11a
hamzashaikh2001 <hamzashaikh2001@hotmail.com>
2023-10-07 20:21:01 -0500

    Added Map and Updated Navbar

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -26,11 +26,13 @@ export default function Home() {
         <div id="contact us" className="flex min-h-screen flex-col items-center justify-start p-44 space-y-6">
           <h2 className="text-4xl">Contact Us</h2>
           <h3 className="text-2xl">Get in contact with us</h3>
-          <h3 className="text-2xl">Phone</h3>
-          <Link href="tel:2815756700">(281) 575-6700</Link>
-          <h3 className="text-2xl">Email</h3>
-          <Link href="mailto:fake@temp.com">fake@temp.com</Link>
-          <p></p>
+          <div>
+            <h3 className="text-2xl">Phone</h3>
+            <Link href="tel:2815756700">(281) 575-6700</Link>
+            <h3 className="text-2xl">Email</h3>
+            <Link href="mailto:fake@temp.com">fake@temp.com</Link>
+          </div>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3466.5682662450795!2d-95.5787357!3d29.674300100000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640dd5e4b7ea63f%3A0x9ed33066f5fbaf93!2sBissonnet%20Medical%20Center!5e0!3m2!1sen!2sus!4v1696726547260!5m2!1sen!2sus" width="600" height="450" style={{border: 0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </main>
     </body>
--- webapp/src/components/Navbar.tsx
+++ webapp/src/components/Navbar.tsx
@@ -1,18 +1,24 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
-
 
 const Navbar = () => {
+  const handleItemClick = (pageName: string) => {
+    const targetElement = document.getElementById(pageName);
+    if (targetElement) {
+      targetElement.scrollIntoView({behavior: "smooth", block: "start"});
+    }
+  }
   return (
     <nav className="fixed w-full h-24 shadow-xl bg-white">
     <div className="flex justify-between items-center h-full w-full px-4 2xl:px-16 text-black">
       <Image src="/vercel.svg" alt="Logo" width={100} height={24} priority/>
-      <ul className="hidden sm:flex uppercase text-black text-xl">
-        <li className="ml-10 hover:border-b"><Link href="https://example.com">Home</Link></li>
-        <li className="ml-10 hover:border-b"><Link href="https://example.com">About</Link></li>
-        <li className="ml-10 hover:border-b"><Link href="https://example.com">Services</Link></li>
-        <li className="ml-10 hover:border-b"><Link href="https://example.com">Contact Us</Link></li>
+      <ul className="hidden sm:flex uppercase text-black text-xl"> 
+        <li className="ml-10 hover:border-b" onClick={() => handleItemClick("home")}>Home</li>
+        <li className="ml-10 hover:border-b" onClick={() => handleItemClick("about")}>About</li>
+        <li className="ml-10 hover:border-b" onClick={() => handleItemClick("services")}>Services</li>
+        <li className="ml-10 hover:border-b" onClick={() => handleItemClick("contact us")}>Contact Us</li>
       </ul>
     </div>
   </nav>
