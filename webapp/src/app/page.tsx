9d1555e84a5b4b46402f23e54db2173e62b9811f
hamzashaikh2001 <hamzashaikh2001@hotmail.com>
2023-10-12 17:24:07 -0500

    Created Services Component

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -4,19 +4,26 @@ import Intro from '@/components/Intro'
 import Navbar from '@/components/Navbar'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Roboto } from 'next/font/google';
+import Services from '@/components/Services'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function Home() {
   return (
-    <body>
+    <body className={roboto.className}>
       <header>
         <Navbar />
       </header>
       <main>
         <Intro />
         <About />
-        <div id="services" className="flex min-h-screen flex-col items-center justify-start p-44 space-y-6 bg-white text-black">
-          <h2 className="text-4xl">Services</h2>
-        </div>
+        <Services />
         <Contact />
       </main>
       <footer id="footer" className="">
@@ -26,6 +33,8 @@ export default function Home() {
 }
 
 /*
+style={{ fontWeight: 700 }}
+
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
---
+++ webapp/src/components/Services.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <div id="services" className="flex min-h-screen flex-col items-center justify-start p-44 space-y-6 bg-white text-black">
+            <h2 className="text-4xl">Services</h2>
+        </div>
+    )
+}
+
+export default Services
