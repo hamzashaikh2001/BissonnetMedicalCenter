973f307fc6000282f2917cf2173166afe4f94b84
hamzashaikh2001 <hamzashaikh2001@hotmail.com>
2023-11-02 17:52:34 -0500

    Carousel Movement Update

--- webapp/src/components/Carousel.tsx
+++ webapp/src/components/Carousel.tsx
@@ -2,27 +2,18 @@
 import React from 'react'
 
 const Carousel = () => {
-    var previous = "CarouselButton1"
-    const handleItemClick = (pageName: string, buttonName: string) => {
-        const targetElement = document.getElementById(pageName);
+    const handleItemClick = (c: number) => {
+        var targetElement = document.getElementById("item" + c);
         if (targetElement) {
             targetElement.scrollIntoView({behavior: "smooth", block: "start"});
         }
-        const prevElement = document.getElementById(previous)
-        if (prevElement) {
-            prevElement.style.backgroundColor = "#64748b";
+        for (let i = 1; i <= 4; i++) {
+            targetElement = document.getElementById("CarouselButton" + i);
+            if (targetElement) {
+                targetElement.style.backgroundColor = (i == c) ? 'white' : '#64748b';
+            }
         }
-        const buttonElement = document.getElementById(buttonName);
-        if (buttonElement) {
-            buttonElement.style.backgroundColor = "white";
-        }
-        previous = buttonName
     }
-    /*
-    const interval = setInterval(() => {
-        handleItemClick("Item".concat(), "CarouselButton".concat("1"));
-      }, 6000);
-    */
     return (
         <div className="carousel h-screen w-full">
             <div id="item1" className="carousel-item w-full">
@@ -38,16 +29,16 @@ const Carousel = () => {
                 <img src="Slides/Slide4.jpg" className="w-full pt-20" />
             </div>
             <div className="flex items-end justify-center absolute h-full w-full z-10 pb-6 gap-2">
-                <div className="w-8 h-5 cursor-pointer flex items-center" onClick={() => handleItemClick("item1", "CarouselButton1")}>
+                <div className="w-8 h-5 cursor-pointer flex items-center" onClick={() => handleItemClick(1)}>
                     <a id="CarouselButton1" className="w-8 h-1 bg-white"></a>
                 </div>
-                <div className="w-8 h-5 cursor-pointer flex items-center" onClick={() => handleItemClick("item2", "CarouselButton2")}>
+                <div className="w-8 h-5 cursor-pointer flex items-center" onClick={() => handleItemClick(2)}>
                     <a id="CarouselButton2" className="w-8 h-1 bg-slate-500"></a>
                 </div>
-                <div className="w-8 h-5 cursor-pointer flex items-center" onClick={() => handleItemClick("item3", "CarouselButton3")}>
+                <div className="w-8 h-5 cursor-pointer flex items-center" onClick={() => handleItemClick(3)}>
                     <a id="CarouselButton3" className="w-8 h-1 bg-slate-500"></a>
                 </div>
-                <div className="w-8 h-5 cursor-pointer flex items-center" onClick={() => handleItemClick("item4", "CarouselButton4")}>
+                <div className="w-8 h-5 cursor-pointer flex items-center" onClick={() => handleItemClick(4)}>
                     <a id="CarouselButton4" className="w-8 h-1 bg-slate-500"></a>
                 </div>
             </div>
