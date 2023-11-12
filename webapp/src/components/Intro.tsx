27af2a7da9a5083ab2281f93aebb816a18e2fe86
hamzashaikh2001 <hamzashaikh2001@hotmail.com>
2023-11-12 15:55:47 -0600

    Color Modification

--- webapp/src/components/Intro.tsx
+++ webapp/src/components/Intro.tsx
@@ -4,9 +4,9 @@ import Carousel from './Carousel'
 const Intro = () => {
     return (
         <div id="home" className="h-screen bg-[url('/background.webp')]">
-            <div className="flex flex-col items-start justify-center absolute h-full z-10 px-8 sm:px-16 space-y-6 text-black">
-                <h1 className="text-6xl font-extrabold w-44 sm:w-full">Your <span className="text-rose-800">Health</span> is Our <span className="text-green-800">Priority</span></h1>
-                <p className="text-xl font-bold">For over twenty-five years, our clinic has strived to deliver quality healthcare at the most affordable rates.</p>
+            <div className="flex flex-col items-start justify-center absolute h-full z-10 px-8 sm:px-16 space-y-6">
+                <h1 className="text-6xl font-extrabold w-44 sm:w-full text-[#2E024E] xs:text-black">Your <span className="text-rose-800">Health</span> is Our <span className="text-green-800">Priority</span></h1>
+                <p className="text-xl font-bold text-white xs:text-black">For over twenty-five years, our clinic has strived to deliver quality healthcare at the most affordable rates.</p>
             </div>
             <div className="hidden md:block">
                 <Carousel />
